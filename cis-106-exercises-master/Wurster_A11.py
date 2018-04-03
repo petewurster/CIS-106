f39ef9a4e805f1a378ddeac2820e95667509d1c7
petewurster <petewurster@yahoo.com>
2018-04-03 18:56:25 -0400

    del

--- cis-106-exercises-master/Wurster_A11.py
+++ cis-106-exercises-master/Wurster_A11.py
@@ -1,13 +1,18 @@
+'''
+customer spec: tip calculator with percentage slider; buttons to calculate,
+        reset, and quit
+added feature: code marked ### (at end of line) is for optional feature to
+        include PA sales tax in the calculated total; tax is only levied on
+        the amount input by the user; to enable this feature, remove ###
+        from left margin
+'''
 
 import tkinter as tki
 
-
-
 def assignment11():
-    global main
     main=tki.Tk()
     main.wm_title('Tip Calculator')
-    main.geometry('242x150')
+    main.geometry('242x166')
 
     #build/pack entry frame
     fr_entry=tki.Frame(main)
@@ -39,25 +44,43 @@ def assignment11():
     calc_btn.pack(side='left')
     reset_btn.pack(side='left')
     quit_btn.pack(side='left')
+
+    #build/pack check box to switch on tax###
+    fr_switch=tki.Frame(main)###
+    global switch###
+    global switch_text###
+    switch_text=tki.StringVar()###
+    switch=tki.IntVar()###
+    tax_switch=tki.Checkbutton(fr_switch,textvariable=switch_text,###
+                               variable=switch)###
+    tax_switch.pack(side='bottom')###
   
     #pack frames into main window & enter loop
     fr_entry.pack(side='top')
     fr_slide.pack()
     fr_button.pack(side='bottom')
+###    fr_switch.pack(side='left')###
+
+    #set init values
     reset()
-   
     tki.mainloop()
 
 #changes the "total" to 1.x times the entered amound (x comes from slider)
 def calculate():
     total=float(amount.get())*(1+float(slide.get())/100)
+    if switch.get()==1: total+=float(amount.get())*.08###
     total=str(format(total,',.2f'))
     total_text.set('Total Amount:  $ '+total)
+    switch_text.set('include PA sales tax (8%):  $ '+###
+                    str(format(float(amount.get())*.08,',.2f')))###
 
 #reset field, label, and slider position
 def reset():
     slide.set(30)
     total_text.set('Total Amount:')
-    amount.delete(0,'end')      ########## try #####   amount=tki.StringVar()
+    amount.delete(0,'end')
+    switch.set(0)###
+    switch_text.set('include PA sales tax (8%) :  ')###
 
+#run main prog
 assignment11()
